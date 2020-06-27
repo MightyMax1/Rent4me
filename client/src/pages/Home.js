20df418a1745319328b102ab05f6c090e204708d
maxim.or <maximor17s@gmail.com>
2020-06-27 17:03:37 +0300

    homePage  basic styling bootstrap-react

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Card, Button, Container, Row, CardDeck, CardGroup, Col } from 'react-bootstrap'
+
 
 function Home(props) {
 	const [counter, setCounter] = useState(0);
@@ -30,39 +32,53 @@ function Home(props) {
 	console.log('products', products);
 
 	return (
-		<div>
+		<React.Fragment>
 			<div className="search">
 				<input type="text" />
 				<button onClick={onClick}>search {counter}</button>
 			</div>
 
-			<div className="categories" style={{ display: 'flex', flexWrap: 'wrap ', width: '70%', margin: 'auto' }}>
-				{
-					categories.map((category, i) => {
-						return (
-							<div key={i} >
-								<h5>{category.name}</h5>
-								<img src={category.img} width="150" height="200" />
-							</div>
-						);
-					})
-				}
-			</div>
-			<div className="trending"></div>
-			<div className="newest">
-				{
-					products.map((product, i) => {
-						return (
-							<div key={i}>
-								<h6>title:{product.title}</h6>
-								<img src={product.mainImg} width="150" height="200" />
-							</div>
+			<Container className="categories" >
+				<h3>קטגוריות  מוצרים</h3>
+				<Row>
+					{
+						categories.map((category, i) => {
+							return (
+								< Card as={Col} md={3} sm={2} xl={3} style={{ width: '8rem' }}>
+									<Card.Img variant="top" src={category.img} />
+									<Card.Footer>
+										<small className="text-muted">{category.name}</small>
+									</Card.Footer>
+								</Card>
+							)
+						})
+					}
+				</Row>
+			</Container>
+			<Container className="trending"></Container>
+			<Container className="newest">
+				<h3>מוצרים חדשים</h3>
+				<CardGroup>
+					{
+						products.map((product, i) => {
+							return (
+								< Card key={i} style={{ width: '5rem' }}>
+									<Card.Img variant="top" src={product.mainImg} />
+									<Card.Body>
+										<Card.Title>{product.title}</Card.Title>
+									</Card.Body>
+									<Card.Footer>
+										<small className="text-muted"> 27/06/2020 16:35</small>
+									</Card.Footer>
+								</Card>
+							);
+						})
 
-						);
-					})
-				}
-			</div>
-		</div>
+					}
+				</CardGroup>
+			</Container>
+		</React.Fragment>
+
 	);
 }
 
