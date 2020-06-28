f63c84e945043d9dbee485080268ed827a59e22d
maxim.or <maximor17s@gmail.com>
2020-06-28 16:25:28 +0300

    add item design with bootstrap

--- client/src/pages/AddItem.js
+++ client/src/pages/AddItem.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 // history hook for use browser history api
 import { useHistory } from 'react-router-dom';
 
+import { Form, FormGroup, FormLabel, Button, Container, Col, Row, Image } from 'react-bootstrap'
+
 // convert file to base64
 const toBase64 = file =>
 	new Promise((resolve, reject) => {
@@ -22,7 +24,7 @@ function AddItem() {
 		// read data from input that changed (event.target)
 		const { name, value, files } = event.target;
 
-		// handle chang for input name images
+		// handle change for input name images
 		if (name == 'images') {
 			const newImages = [];
 			if (files) {
@@ -44,7 +46,7 @@ function AddItem() {
 		// prevent form refresh page (default behavior of html forms)
 		event.preventDefault();
 
-      // send request ot server add new product 
+		// send request to server add new product 
 		const res = await fetch('http://localhost:4000/products', {
 			method: 'POST',
 			body: JSON.stringify(form),
@@ -59,55 +61,72 @@ function AddItem() {
 
 		console.log('data', data);
 
-      // use history instance for redirect to private page
+		// use history instance for redirect to private page
 		history.push('/private');
 	}
 
 	console.log('form', form);
 	return (
-		<div>
-			<form className="form" onChange={onChange} onSubmit={onSubmit}>
-				<div className="title">
-					title: <input type="text" name="title" />
-				</div>
-				<div className="description">
-					description: <input type="text" name="description" />
-				</div>
-				<div className="category">
-					category:
-					<select name="category" id="">
-						<option value="1">itme</option>
-						<option value="2">itme</option>
-						<option value="3">itme</option>
-					</select>
-				</div>
-
-				<div className="price_hour">
-					price_hour:
-					<input type="text" name="priceHour" />
-				</div>
-				<div className="price_day">
-					price_day:
-					<input type="text" name="priceDay" />
-				</div>
-				<div className="images">
-					images:
-					<input type="file" name="images" multiple />
-				</div>
-				<button>Add Item</button>
+		<Container >
+			<form dir="rtl" className="form" onChange={onChange} onSubmit={onSubmit} >
+				<Container fluid={true} style={{ maxWidth: "430px", }} >
+					<FormGroup as={Row} className="title">
+						<FormLabel  >
+							כותרת:
+						</FormLabel>
+						<Form.Control size="sm" type="text" name="title" placeholder="הכנס כותרת" />
+					</FormGroup>
+					<FormGroup as={Row} className="description">
+						<FormLabel>
+							תיאור מוצר:
+					</FormLabel>
+						<Form.Control size="sm" type="text" name="description" placeholder="הכנס תיאור מוצר" />
+					</FormGroup>
+					<FormGroup as={Row} className="category">
+						<FormLabel>קטגוריה</FormLabel>
+						<Form.Control size="sm" as="select" name="category" id="">
+							<option value="1">itme</option>
+							<option value="2">itme</option>
+							<option value="3">itme</option>
+						</Form.Control>
+					</FormGroup>
+					<Form.Row xl={12}>
+						<FormGroup as={Col} md={6} >
+							<FormLabel>
+								מחיר לפי שעה:
+							</FormLabel>
+							<Form.Control size="sm" type="text" name="priceHour" placeholder='הכנס מחיר לפי שעה בש"ח' />
+						</FormGroup>
+						<FormGroup as={Col} md={6} >
+							<FormLabel>
+								מחיר לפי יום:
+							</FormLabel>
+							<Form.Control size="sm" type="text" name="priceDay" placeholder='הכנס מחיר ליום בש"ח' />
+						</FormGroup>
+					</Form.Row>
+					<FormGroup as={Row} className="images">
+						<FormLabel>
+							הוסף תמונות :
+						</FormLabel>
+						<Form.File name="images" multiple />
+					</FormGroup>
+					<Button type="submit" variant="primary" size="md" block>הוסף מוצר</Button>
+				</Container>
 			</form>
-			<div className="imagesPreviw">
+			<Row className="imagesPreviw">
 				{form.images &&
 					form.images.map((img, i) => {
 						return (
-							<div>
-								<input type="checkbox" />
-								<img width="200" height="200" key={i} src={img} alt="prev" />
-							</div>
+							<FormGroup as={Col}>
+								<Form.Check />
+								<Image width="170" height="180" key={i} src={img} alt="prev" rounded />
+							</FormGroup>
+
 						);
 					})}
-			</div>
-		</div>
+			</Row>
+		</Container>
+
 	);
 }
 
