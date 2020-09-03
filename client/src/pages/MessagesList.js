aa36fa6009ca603e8f74bda8db7e6b76a847037e
maxim.or <maximor17s@gmail.com>
2020-09-03 03:29:53 +0300

    messages list design

--- client/src/pages/MessagesList.js
+++ client/src/pages/MessagesList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { Container, Row, Col, Card, Image } from 'react-bootstrap';
-
 import Api from '../Api';
+
+
 
 const MessagesList = ({ user }) => {
 	const [messages, setMessages] = useState({});
@@ -12,8 +13,6 @@ const MessagesList = ({ user }) => {
 		console.log('use effect');
 
 		async function getChats() {
-			console.log(user);
-
 			let newChats = [];
 			const chats = await Api.getChatsByUserId(user._id);
 			for (const chat of chats) {
@@ -40,22 +39,12 @@ const MessagesList = ({ user }) => {
 		<Container>
 			{chats.map(({ chat, friend }) => {
 				return (
-					<Card key={chat._id} onClick={() => history.push(`messages/message/${chat._id}`)}>
-						<Card.Body as={Row} dir="rtl">
-							<Col md={2} sm={2} xs={2}>
-								<svg
-									width="3em"
-									height="3em"
-									viewBox="0 0 16 16"
-									class="bi bi-person-circle"
-									fill="currentColor"
-									xmlns="http://www.w3.org/2000/svg">
-									<path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
-									<path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-									<path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z" />
-								</svg>
+					<Card key={chat._id} onClick={() => history.push(`messages/message/${chat._id}`)} >
+						<Card.Body as={Row} dir="rtl" className="card-hover">
+							<Col md={1} sm={1} xs={2}>
+								<Image src={friend.profilepPic} style={{ height: "50px", width: "50px" }} roundedCircle />
 							</Col>
-							<Col md={{ span: 2, offset: 5 }} sm={3} xs={5} className="mt-2">
+							<Col md={{ span: 2, offset: 6 }} sm={3} xs={5} className="mt-2">
 								<Card.Title>
 									{friend.firstName} {friend.lastName}
 								</Card.Title>
